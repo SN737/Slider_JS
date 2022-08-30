d32669e22c05c8c9029d65453354ab49fcd962a5
Sergey <92508690+SN737@users.noreply.github.com>
2022-08-30 15:10:43 +0300

    garbage-collector

--- script-2.js
+++ script-2.js
@@ -1,26 +1,34 @@
 const image = document.querySelector('.slider-images');
 const leftBtn = document.querySelector('.nav-arrow__left');
-const rightBtn = document.querySelector('.nav-arrow__right'); 
+const rightBtn = document.querySelector('.nav-arrow__right');
+const city = document.querySelector('.squares__city');
+const apart = document.querySelector('.squares__apart');
+const repaitTime = document.querySelector('.squares__rep-time');
+const costTime = document.querySelector('.squares__rep-cost');
 let pos = -1;
 document.addEventListener('DOMContentLoaded', ()=>{
-    image.innerHTML =  `<img src="./img/img1.png" alt="тут был картинке"></img>`;
+    imgRender(0);
+    // image.innerHTML =  `<img src="./img/img1.png" alt="тут был картинке"></img>`;
     
 });
 
 const sliderArray = [
 {image:'./img/img1.png',
-city: 'Rostov-on-Don LCD Admiral',
-area: '81 m2',
+city: 'Rostov-on-Don ',
+cityPlace: 'LCD Admiral',
+area: '81',
 time: '3.5 months', 
 cost: 'upon request'},
 {image:'./img/img2.png',
-city: 'Sochi Thieves',
-area: '105 m2',
+city: 'Sochi',
+cityPlace: 'Thieves',
+area: '105',
 time: '4 months', 
 cost: 'upon request'},
-{image:'./img/img1.png',
-city: 'Rostov-on-Don Patriotic',
-area: '93m2',
+{image:'./img/img3.png',
+city: 'Rostov-on-Don',
+cityPlace: 'Patriotic',
+area: '93',
 time: '3 months', 
 cost: 'upon request'}];
 
@@ -44,5 +52,10 @@ leftBtn.addEventListener('click', ()=>{
 
 function imgRender(pos){
     // start.remove();      
-    image.innerHTML = `<img src="${sliderArray[pos].image}" alt="тут был картинке"></img>`;
+    image.innerHTML = `<img src="${sliderArray[pos].image}" alt="тут должен быть картинке"></img>`;
+    city.innerHTML = `<h3 class="orange-text">City:</h3><span>${sliderArray[pos].city}<br>{${sliderArray[pos].cityPlace}}</span>`;
+    apart.innerHTML = `<h3 class="orange-text">apartment area:</h3><span>${sliderArray[pos].area} m2</span></div>`;
+    repaitTime.innerHTML = `<h3 class="orange-text">Repair time:</h3><span>${sliderArray[pos].time}</span>`;
+    costTime.innerHTML = `<h3 class="orange-text">Repair Cost:</h3><span>Upon request</span>`;
+
 }
